1a105afebd4a24b91af5d0000ec4a898bf79c9b2
famato46 <f.amato46@studenti.unisa.it>
2025-12-15 14:56:38 +0100

    Update Documentazione doxygen

---
+++ Documentazione/Doxygen/hierarchy.js
@@ -0,0 +1,37 @@
+var hierarchy =
+[
+    [ "Application", null, [
+      [ "it.unisa.diem.softeng.gruppo14.main.Main", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1main_1_1_main.html", null ]
+    ] ],
+    [ "Comparator", null, [
+      [ "it.unisa.diem.softeng.gruppo14.gestionedati.ComparatoreLibri", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_comparatore_libri.html", null ],
+      [ "it.unisa.diem.softeng.gruppo14.gestionedati.ComparatorePrestiti", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_comparatore_prestiti.html", null ],
+      [ "it.unisa.diem.softeng.gruppo14.gestionedati.ComparatoreUtenti", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_comparatore_utenti.html", null ]
+    ] ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionedati.ComparatoreLibriTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_comparatore_libri_test.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionedati.ComparatorePrestitiTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_comparatore_prestiti_test.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionedati.ComparatoreUtentiTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_comparatore_utenti_test.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionefile.FileManager", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionefile_1_1_file_manager.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionefile.FileManagerTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionefile_1_1_file_manager_test.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionelogica.GestioneLibri", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionelogica_1_1_gestione_libri.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionelogica.GestioneLibriTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionelogica_1_1_gestione_libri_test.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionelogica.GestionePrestiti", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionelogica_1_1_gestione_prestiti.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionelogica.GestionePrestitiTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionelogica_1_1_gestione_prestiti_test.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionelogica.GestioneUtenti", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionelogica_1_1_gestione_utenti.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionelogica.GestioneUtentiTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionelogica_1_1_gestione_utenti_test.html", null ],
+    [ "Initializable", null, [
+      [ "it.unisa.diem.softeng.gruppo14.gestioneinterfaccia.LibroController", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestioneinterfaccia_1_1_libro_controller.html", null ],
+      [ "it.unisa.diem.softeng.gruppo14.gestioneinterfaccia.NuovoPrestitoController", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestioneinterfaccia_1_1_nuovo_prestito_controller.html", null ],
+      [ "it.unisa.diem.softeng.gruppo14.gestioneinterfaccia.PrestitoAttivoController", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestioneinterfaccia_1_1_prestito_attivo_controller.html", null ],
+      [ "it.unisa.diem.softeng.gruppo14.gestioneinterfaccia.SchermataPrincipaleController", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestioneinterfaccia_1_1_schermata_principale_controller.html", null ],
+      [ "it.unisa.diem.softeng.gruppo14.gestioneinterfaccia.UtenteController", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestioneinterfaccia_1_1_utente_controller.html", null ]
+    ] ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionedati.LibroTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html", null ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionedati.PrestitoTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_prestito_test.html", null ],
+    [ "Serializable", null, [
+      [ "it.unisa.diem.softeng.gruppo14.gestionedati.Libro", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro.html", null ],
+      [ "it.unisa.diem.softeng.gruppo14.gestionedati.Prestito", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_prestito.html", null ],
+      [ "it.unisa.diem.softeng.gruppo14.gestionedati.Utente", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_utente.html", null ]
+    ] ],
+    [ "it.unisa.diem.softeng.gruppo14.gestionedati.UtenteTest", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_utente_test.html", null ]
+];
