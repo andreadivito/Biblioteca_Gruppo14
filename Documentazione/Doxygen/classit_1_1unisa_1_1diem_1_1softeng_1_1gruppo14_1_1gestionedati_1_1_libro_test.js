4e77db06854c34ed0a9fa93f7cb3b5aadee14a62
andreadivito <andreadivito2005@gmail.com>
2025-12-16 00:00:43 +0100

    Update Documentazione Doxygen

---
+++ Documentazione/Doxygen/classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.js
@@ -0,0 +1,16 @@
+var classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test =
+[
+    [ "testDecrementaCopie", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#adafe242c76c64ad81285128becb495eb", null ],
+    [ "testGetAnnoPubblicazione", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#aa8425559032291889096b5ceb5d786c4", null ],
+    [ "testGetAutori", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#a0f5a73b5b84207a15cf8d2450a64ceb8", null ],
+    [ "testGetISBN", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#a329ef184876c38a6069d2bd4d5b77f67", null ],
+    [ "testGetNumCopie", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#a096beecd3a9c1b26f4573877f9ef9c29", null ],
+    [ "testGetTitolo", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#acfc1ca9d7c15698f6e9e882d9a0221a0", null ],
+    [ "testIncrementaCopie", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#a0b9581d3bf83d0cd2845a260a8744b15", null ],
+    [ "testSetAnnoPubblicazione", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#a62215070ce13cd45a8ea3519ffdec959", null ],
+    [ "testSetAutori", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#a1dea7173e24953b88525fd4ab626d7f3", null ],
+    [ "testSetISBN", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#a09df6dc2a971efe914311571e5afc61a", null ],
+    [ "testSetNumCopie", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#a181b7e178c44fafb31d0398619c8ce1b", null ],
+    [ "testSetTitolo", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#ab162debc2df1433f6f1f3451c56dd80e", null ],
+    [ "testToString", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_libro_test.html#aad8cd2736a2a6604a563694125650f8d", null ]
+];
