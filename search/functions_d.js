782d0cbb071f21429ca04d669cfb607629ca57ae
famato46 <f.amato46@studenti.unisa.it>
2025-12-15 16:30:51 +0100

    Delete search directory

--- search/functions_d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['utente_0',['Utente',['../classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_utente.html#a91d692e1d42f694417934e790c402a04',1,'it::unisa::diem::softeng::gruppo14::gestionedati::Utente']]]
-];
--- search/functions_e.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['verificaritardo_0',['verificaRitardo',['../classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_prestito.html#ac0bc5bb086aee52c6e6d0c5389505c0e',1,'it::unisa::diem::softeng::gruppo14::gestionedati::Prestito']]]
-];
