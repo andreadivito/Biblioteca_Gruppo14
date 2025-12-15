20e70e9c5c8cde429703ef340b34fcc80f4854b1
famato46 <f.amato46@studenti.unisa.it>
2025-12-15 16:31:51 +0100

    Update

---
+++ Documentazione/Doxygen/search/functions_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['registraprestito_0',['registraPrestito',['../classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionelogica_1_1_gestione_prestiti.html#a360f8077b5539811afacbfbc73b22ee4',1,'it::unisa::diem::softeng::gruppo14::gestionelogica::GestionePrestiti']]],
+  ['registrarestituzione_1',['registraRestituzione',['../classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionelogica_1_1_gestione_prestiti.html#a7193239c7f585c6f3a3db781abd481a1',1,'it::unisa::diem::softeng::gruppo14::gestionelogica::GestionePrestiti']]]
+];
---
+++ Documentazione/Doxygen/search/functions_d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['utente_0',['Utente',['../classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo14_1_1gestionedati_1_1_utente.html#a91d692e1d42f694417934e790c402a04',1,'it::unisa::diem::softeng::gruppo14::gestionedati::Utente']]]
+];
